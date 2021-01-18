af57ceab64763a00df081235af5fdba5b221b2e3
David Hernandez <dh.valencia.work@outlook.com>
2021-01-18 11:30:10 -0600

    Se elimina codigo comentado

--- Funcionalidad/Config/ActualizarAppSetting.cs
+++ Funcionalidad/Config/ActualizarAppSetting.cs
@@ -31,34 +31,7 @@ namespace Analizador.Funcionalidad.Config
             }
             else
                 parametro.nuevosvalues.AppendLine(parametro.item);
-
-            //bool actualizar = false;
-
-            //var key = parametro.item.Split(new string[] { "key" }, StringSplitOptions.RemoveEmptyEntries);
-
-            //actualizar = !(key?.Length == 1);
-
-            //if (actualizar)
-            //{
-            //    var value = key[1].Split(new string[] { "value" }, StringSplitOptions.RemoveEmptyEntries);
-
-            //    if (value?.Length > 1)
-            //    {
-            //        string llave = value[0].Substring(1, value[0].Length - 1).Replace("\"", "").Trim();
-            //        var appsetting = parametro.configparseado.appSetting.Find(i => i.key.Trim() == llave);
-
-            //        actualizar = !string.IsNullOrWhiteSpace(appsetting?.key);
-
-            //        if (actualizar)
-            //            parametro.nuevosvalues.AppendLine($"<add key=\"{ appsetting.key }\" value=\"{ appsetting.value }\"/>");
-            //        else
-            //            parametro.nuevosvalues.AppendLine(parametro.item);
-            //    }
-            //}
-            //else
-            //    parametro.nuevosvalues.AppendLine(parametro.item);
-
-
+           
         }
     }
 }
